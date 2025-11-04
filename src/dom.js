0a58e024af9009ad9fbaac8153b7e5f382e8b5b4
blarrise <61951559+blariise@users.noreply.github.com>
2025-11-05 00:12:26 +0100

    Add icons and render weather data

--- src/dom.js
+++ src/dom.js
@@ -14,25 +14,29 @@ export default function createCardDOM() {
 
 function createCardUpperDOM() {
   const upperDiv = document.createElement("div");
+
   upperDiv.className = "upper-card";
   const simpleInfoDiv = createSimpleInfoDOM();
+
+  const placeDiv = document.createElement("div");
+  placeDiv.className = "place";
+
   upperDiv.appendChild(simpleInfoDiv);
+  upperDiv.appendChild(placeDiv);
   return upperDiv;
 }
 
 function createSimpleInfoDOM() {
   const simpleInfoDiv = document.createElement("div");
   simpleInfoDiv.className = "simple-info";
-  
+
   const dateDiv = document.createElement("div");
   dateDiv.className = "date";
   const iconDiv = document.createElement("div");
   iconDiv.className = "icon";
   const tempDiv = document.createElement("div");
   tempDiv.className = "temperature";
-  const placeDiv = document.createElement("div");
-  placeDiv.className = "place";
-  
+
   simpleInfoDiv.appendChild(dateDiv);
   simpleInfoDiv.appendChild(iconDiv);
   simpleInfoDiv.appendChild(tempDiv);
@@ -41,13 +45,65 @@ function createSimpleInfoDOM() {
 
 function createInfoDOM() {
   const infoDiv = document.createElement("div");
-  infoDiv.className = "lower-card";
+  infoDiv.className = "info";
+
+  const relativeHumidity = document.createElement("div");
+  relativeHumidity.className = "relative-humidity";
+  const cloudCover = document.createElement("div");
+  cloudCover.className = "cloud-cover";
+  const visibility = document.createElement("div");
+  visibility.className = "visibility";
+  const precipitation = document.createElement("div");
+  precipitation.className = "precipitation";
+  const precipitationChance = document.createElement("div");
+  precipitationChance.className = "precipitation-chance";
+  const precipitationType = document.createElement("div");
+  precipitationType.className = "precipitation-type";
+  const snowfall = document.createElement("div");
+  snowfall.className = "snowfall";
+  const snowDepth = document.createElement("div");
+  snowDepth.className = "snow-depth";
+  const windSpeed = document.createElement("div");
+  windSpeed.className = "wind-speed";
+  const windGust = document.createElement("div");
+  windGust.className = "wind-gust";
+  const windDirection = document.createElement("div");
+  windDirection.className = "wind-direction";
+  const solarRadiation = document.createElement("div");
+  solarRadiation.className = "solar-radiation";
+  const solarEnergy = document.createElement("div");
+  solarEnergy.className = "solar-energy";
+  const uvIndex = document.createElement("div");
+  uvIndex.className = "uv-index";
+
+  infoDiv.appendChild(relativeHumidity);
+  infoDiv.appendChild(cloudCover);
+  infoDiv.appendChild(visibility);
+  infoDiv.appendChild(precipitation);
+  infoDiv.appendChild(precipitationChance);
+  infoDiv.appendChild(precipitationType);
+  infoDiv.appendChild(snowfall);
+  infoDiv.appendChild(snowDepth);
+  infoDiv.appendChild(windSpeed);
+  infoDiv.appendChild(windGust);
+  infoDiv.appendChild(windDirection);
+  infoDiv.appendChild(solarRadiation);
+  infoDiv.appendChild(solarEnergy);
+  infoDiv.appendChild(uvIndex);
+
   return infoDiv;
 }
 
 function createCarouselDOM() {
   const carouselDiv = document.createElement("div");
   carouselDiv.className = "carousel";
+
+  for(let i = 0; i < 4; ++i) {
+    const carouselElement = document.createElement("div");
+    carouselElement.className = "carousel-element";
+    carouselDiv.appendChild(carouselElement);
+  }
+
   return carouselDiv;
 }
 
--- src/index.js
+++ src/index.js
@@ -9,17 +9,69 @@ import createCardDOM from "./dom.js";
     if (event.key == "Enter" && search.value !== "") {
       weatherData = await fetchWeather(search.value);
       search.value = "";
+      console.log(weatherData);
       renderCardDOM();
-      renderWeatherData(weatherData);
+      renderWeatherData(weatherData.currentConditions, weatherData.resolvedAddress);
     }
   });
 })();
 
-function renderWeatherData(weatherData) {
-}
-
 function renderCardDOM() {
   const weatherContainer = document.querySelector(".weather-container");
   weatherContainer.appendChild(createCardDOM());
+}
+
+function renderUpperCard(weatherData, place) {
+  const dateDiv = document.querySelector(".date");
+  dateDiv.innerText = weatherData.datetime;
+
+  const iconDiv = document.querySelector(".icon");
+  import(`./${weatherData.icon}.svg`).then((module) => {
+    const icon = module.default;
+    iconDiv.innerHTML = icon;
+  });
+
+  const temperature = document.querySelector(".temperature");
+  temperature.innerText = weatherData.temp;
+
+  const placeDiv = document.querySelector(".place");
+  placeDiv.innerText = place;
+}
+
+function renderInfo(weatherData) {
+  const relativeHumidity = document.querySelector(".relative-humidity");
+  const cloudCover = document.querySelector(".cloud-cover");
+  const visibility = document.querySelector(".visibility");
+  const precipitation = document.querySelector(".precipitation");
+  const precipitationChance = document.querySelector(".precipitation-chance");
+  const precipitationType = document.querySelector(".precipitation-type");
+  const snowfall = document.querySelector(".snowfall");
+  const snowDepth = document.querySelector(".snow-depth");
+  const windSpeed = document.querySelector(".wind-speed");
+  const windGust = document.querySelector(".wind-gust");
+  const windDirection = document.querySelector(".wind-direction");
+  const solarRadiation = document.querySelector(".solar-radiation");
+  const solarEnergy = document.querySelector(".solar-energy");
+  const uvIndex = document.querySelector(".uv-index");
+
+  relativeHumidity.innerText = `Relative humidity: ${weatherData.humidity}`;
+  cloudCover.innerText = `Cloud cover: ${weatherData.cloudcover}`;
+  visibility.innerText = `Visibility: ${weatherData.visibility}`;
+  precipitation.innerText = `Precipitation: ${weatherData.precip}`;
+  precipitationChance.innerText = `Precipitation chance: ${weatherData.precipprob}`;
+  precipitationType.innerText = `Precipitation type: ${weatherData.preciptype}`;
+  snowfall.innerText = `Snowfall: ${weatherData.snow}`;
+  snowDepth.innerText = `Snow depth: ${weatherData.snowdepth}`;
+  windSpeed.innerText = `Wind speed: ${weatherData.windspeed}`;
+  windGust.innerText = `Wind gust: ${weatherData.windgust}`;
+  windDirection.innerText = `Wind direction: ${weatherData.winddir}`;
+  solarRadiation.innerText = `Solar radiation: ${weatherData.solarradiation}`;
+  solarEnergy.innerText = `Solar energy: ${weatherData.solarenergy}`;
+  uvIndex.innerText = `UV index: ${weatherData.uvindex}`;
+}
+
+function renderWeatherData(weatherData, place) {
+  renderUpperCard(weatherData, place);
+  renderInfo(weatherData);
 }
 
